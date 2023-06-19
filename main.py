e1e86d2e9caae74ed6709b95255c20fb06747343
Prathamesh Gujar <gujarpa@rknec.edu>
2023-06-19 09:35:15 +0530

    adding main file

---
+++ main.py
@@ -0,0 +1,48 @@
+import cv2
+
+
+def faceBox(faceNet, frame):
+
+    frameHeight = frame.shape[0]
+    frameWidth = frame.shape[1]
+
+
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (277,277), [104,117,123], swapRB = False)
+    faceNet.setInput(blob)
+    detection = faceNet.forward()
+
+    bbox = []
+    for i in range(detection.shape[2]):    #shape is (1,1,200,7) hence accessing 2nd index i.e 200
+        confidence  = detection[0,0,i,2]
+        if confidence > 0.7:
+            x1 = int (detection[0,0,i,3]*frameWidth)
+            y1 = int (detection[0,0,i,4]*frameWidth)
+            x2 = int (detection[0,0,i,5]*frameWidth)
+            y2 = int (detection[0,0,i,6]*frameWidth)
+            bbox.append([x1,y1,x2,y2])
+            cv2.rectangle(frame, (x1,y1), (x2,y2), (0,255,0), 1)  #(0,255,0) means green color
+    return detection
+
+
+
+##path for faceProto and faceModel
+faceProto = "opencv_face_detector.pbtxt"
+faceModel = "opencv_face_detector_uint8.pb"
+
+
+faceNet = cv2.dnn.readNet(faceModel, faceProto)
+
+
+video = cv2.VideoCapture(0)
+
+while True:
+    ret,frame = video.read()
+
+    detect = faceBox(faceNet, frame)
+    cv2.imshow("Age-Gender-Identification", frame)
+    k = cv2.waitKey(100)
+    
+    if k == ord('q'):
+        break
+video.release()
+cv2.destroyAllWindows()
