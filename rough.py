04552098c1efb8ee194d517f7235f77ae1085b37
Prathamesh Gujar <gujarpa@rknec.edu>
2023-07-03 23:28:35 +0530

    adding new models and other changer

--- rough.py
+++ rough.py
@@ -1,72 +1,85 @@
-import cv2
+# import cv2
+# import numpy as np
+
+# def faceBox(faceNet, frame):
+
+#     frameHeight = frame.shape[0]
+#     frameWidth = frame.shape[1]
 
 
-def faceBox(faceNet,frame):
-    frameHeight=frame.shape[0]
-    frameWidth=frame.shape[1]
-    blob=cv2.dnn.blobFromImage(frame, 1.0, (300,300), [104,117,123], swapRB=False)
-    faceNet.setInput(blob)
-    detection=faceNet.forward()
-    bboxs=[]
-    for i in range(detection.shape[2]):
-        confidence=detection[0,0,i,2]
-        if confidence>0.7:
-            x1=int(detection[0,0,i,3]*frameWidth)
-            y1=int(detection[0,0,i,4]*frameHeight)
-            x2=int(detection[0,0,i,5]*frameWidth)
-            y2=int(detection[0,0,i,6]*frameHeight)
-            bboxs.append([x1,y1,x2,y2])
-            cv2.rectangle(frame, (x1,y1),(x2,y2),(0,255,0), 1)
-    return frame, bboxs
+#     blob = cv2.dnn.blobFromImage(frame, 1.0, (277,277), [104,117,123], swapRB = False)
+#     faceNet.setInput(blob)
+#     detection = faceNet.forward()
 
-
-faceProto = "opencv_face_detector.pbtxt"
-faceModel = "opencv_face_detector_uint8.pb"
-
-ageProto = "age_deploy.prototxt"
-ageModel = "age_net.caffemodel"
-
-genderProto = "gender_deploy.prototxt"
-genderModel = "gender_net.caffemodel"
+#     bboxs = []
+#     for i in range(detection.shape[2]):    #shape is (1,1,200,7) hence accessing 2nd index i.e 200
+#         confidence  = detection[0,0,i,2]
+#         if confidence > 0.7:
+#             x1 = int (detection[0,0,i,3]*frameWidth)
+#             y1 = int (detection[0,0,i,4]*frameHeight)
+#             x2 = int (detection[0,0,i,5]*frameWidth)
+#             y2 = int (detection[0,0,i,6]*frameHeight)
+#             bboxs.append([x1,y1,x2,y2])
+#             cv2.rectangle(frame, (x1,y1), (x2,y2), (0,255,0), 1)  #(0,255,0) means green color
+#     return frame, bboxs
 
 
 
-faceNet=cv2.dnn.readNet(faceModel, faceProto)
-ageNet=cv2.dnn.readNet(ageModel,ageProto)
-genderNet=cv2.dnn.readNet(genderModel,genderProto)
-
-MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
-ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
-genderList = ['Male', 'Female']
-
-
-video=cv2.VideoCapture('4.mp4')
-
-padding=20
-
-while True:
-    ret,frame=video.read()
-    frame,bboxs=faceBox(faceNet,frame)
-    for bbox in bboxs:
-        # face=frame[bbox[1]:bbox[3], bbox[0]:bbox[2]]
-        face = frame[max(0,bbox[1]-padding):min(bbox[3]+padding,frame.shape[0]-1),max(0,bbox[0]-padding):min(bbox[2]+padding, frame.shape[1]-1)]
-        blob=cv2.dnn.blobFromImage(face, 1.0, (227,227), MODEL_MEAN_VALUES, swapRB=False)
-        genderNet.setInput(blob)
-        genderPred=genderNet.forward()
-        gender=genderList[genderPred[0].argmax()]
+# ##path for faceProto and faceModel
+# faceProto="opencv_face_detector.pbtxt"
+# faceModel="opencv_face_detector_uint8.pb"
+# ageProto="age_deploy.prototxt"
+# ageModel="age_net.caffemodel"
+# genderProto="gender_deploy.prototxt"
+# genderModel="gender_net.caffemodel"
 
 
-        ageNet.setInput(blob)
-        agePred=ageNet.forward()
-        age=ageList[agePred[0].argmax()]
+
+# faceNet = cv2.dnn.readNet(faceModel, faceProto)
+# ageNet = cv2.dnn.readNet(ageModel, ageProto)
+# genderNet = cv2.dnn.readNet(genderModel, genderProto)
+
+# MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+
+# ##agelist taken from google
+# ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+# genderList = ['Male', 'Female']
+
+# video = cv2.VideoCapture(0)
+
+# while True:
+#     ret,frame = video.read() 
+
+#     frame, bboxs = faceBox(faceNet, frame)
+#     for bbox in bboxs:
+#         face = frame[bbox[1]:bbox[3], bbox[0]:bbox[2]]      ##to cut face from -> bboxs.append([x1,y1,x2,y2])
+
+#         blob = cv2.dnn.blobFromImage(face, 1.0, (277,277), MODEL_MEAN_VALUES, swapRB = False)
+#         blob = blob.astype(np.float32)
+
+#         srcMat = cv2.dnn.blobFromImage(face, 1.0, (277,277), MODEL_MEAN_VALUES, swapRB = False)
+#         weights = cv2.dnn.blobFromImage(face, 1.0, (277,277), MODEL_MEAN_VALUES, swapRB = False)
+
+#         genderNet.setInput(blob)
+#         genderPredictions = genderNet.forward()
+#         # gender = genderList[genderPredictions[0].argmax()]
+#         if genderPredictions.size != 0:
+#             gender = genderList[genderPredictions[0].argmax()]
+#         else:
+#             gender = "Unknown"
 
 
-        label="{},{}".format(gender,age)
-        cv2.rectangle(frame,(bbox[0], bbox[1]-30), (bbox[2], bbox[1]), (0,255,0),-1) 
-        cv2.putText(frame, label, (bbox[0], bbox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255,255,255), 2,cv2.LINE_AA)
-    cv2.imshow("Age-Gender",frame)
-    k=cv2.waitKey(1)
-    if k==ord('q'):
-        break
-video.release()
-cv2.destroyAllWindows()
+#         ageNet.setInput(blob)
+#         agePredictions = ageNet.forward()
+#         age = ageList[agePredictions[0].argmax()]
+
+#         label = "{},{}".format(gender, age)
+#         cv2.putText(frame, label, (bbox[0], bbox[1]-10), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255,255,255), 2)
+
+#     cv2.imshow("Age-Gender-Identification", frame)
+#     k = cv2.waitKey(100)
+    
+#     if k == ord('q'):
+#         break
+# video.release()
+# cv2.destroyAllWindows()
